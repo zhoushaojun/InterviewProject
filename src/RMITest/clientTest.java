b9f91fcc7cbfe3228b0c8118913ff4e0e49fcedc
zhoushaojun <zhoushaojun@sjzhou-cpyf-pc>
2014-08-19 13:31:38 +0800

    1

--- src/RMITest/clientTest.java
+++ src/RMITest/clientTest.java
@@ -1,6 +1,8 @@
 package RMITest;
 
 import java.rmi.Naming;
+import java.util.ArrayList;
+import java.util.List;
 
 public class clientTest {
 	public static void main(String[] args) {
@@ -13,5 +15,24 @@ public class clientTest {
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
+	}
+	
+	public void test(){
+		List<Integer> my = new ArrayList<Integer>();
+		my.add(1);
+		my.add(3);my.add(2);
+		
+		
+		List<String> my2 = new ArrayList<String>();
+		my2.add("1");
+		my2.add("2");
+		//my2.add("3");
+		
+		System.out.println(getsize(my));
+		System.out.println(getsize(my2));
+	}
+	public static int getsize(List obj){
+		//obj.add("e");
+		return obj.size();
 	}
 }
