8b947381b017e19409f1d8d661f3cb180e26c9bf
zhoushaojun <zhoushaojun@sjzhou-cpyf-pc>
2014-09-01 15:31:26 +0800

    Add Reflection

--- src/JavaOptimize/RandAccessTest.java
+++ src/JavaOptimize/RandAccessTest.java
@@ -3,22 +3,27 @@ package JavaOptimize;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.nio.ByteBuffer;
+import java.lang.reflect.Method;
+import java.nio.IntBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 
 public class RandAccessTest {
-
 	public static void write() {
 		RandomAccessFile raf = null;
 		try {
 			// 创建一个可读写的对象
 			raf = new RandomAccessFile("D:\\raf.dat", "rw");
 			// 写入数据。 可直接操作指定基本数据类型的对象。
-			raf.writeInt(12345);
-			raf.writeBytes("java hello");
-		
-			raf.writeBytes("where are you");
+			raf.writeInt(10);
+			raf.writeInt(Integer.MAX_VALUE);
+			// raf.writeChars("zhou绍钧");
+			// raf.writeChars("leng竟 的d");
+			// raf.writeChars("java");
+			// raf.writeChars("hello world");
+			// raf.writeBytes("java hello");
+
+			// raf.writeBytes("where are you");
 			raf.writeInt(22);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -44,12 +49,12 @@ public class RandAccessTest {
 			// raf.write("datou".getBytes());
 
 			// 可直接操作指针，灵活的访问内容
-//			int value = raf.readInt();
-//			System.out.println(value);
+			// int value = raf.readInt();
+			// System.out.println(value);
 			raf.seek(4);
 			byte b[] = new byte[10];
 			raf.read(b);
-			//System.out.println(new String(b));
+			// System.out.println(new String(b));
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
@@ -62,29 +67,62 @@ public class RandAccessTest {
 		}
 	}
 
-	public static void bufferMap(){
+	public static void bufferWrite() throws IOException {
+		RandomAccessFile raf;
+		try {
+			raf = new RandomAccessFile("D:\\raf.dat", "rw");
+			FileChannel fc = raf.getChannel();
+			MappedByteBuffer mappedByteBuffer = fc.map(FileChannel.MapMode.READ_WRITE, 0, 8);
+			// CharBuffer charBuffer = mappedByteBuffer.asCharBuffer();
+			// charBuffer.put("周绍钧");
+			// charBuffer.put("leng");
+			IntBuffer intBuffer = mappedByteBuffer.asIntBuffer();
+			intBuffer.put(10);
+			intBuffer.put(Integer.MAX_VALUE);
+			fc.close();
+		} catch (FileNotFoundException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void bufferMap() {
 		try {
 			RandomAccessFile raf = new RandomAccessFile("D:\\raf.dat", "rw");
 			FileChannel fc = raf.getChannel();
-			MappedByteBuffer mappedByteBuffer = fc.map(FileChannel.MapMode.READ_WRITE,0, raf.length());
-			
-			int value = mappedByteBuffer.getInt();
-				System.out.println(value);
-				byte[] byts = new byte[10];
-				ByteBuffer bf = mappedByteBuffer.get(byts);
-				System.out.println(new String(byts));
-			
+			MappedByteBuffer mappedByteBuffer = fc.map(FileChannel.MapMode.READ_WRITE, 0, raf.length());
+
+			// IntegerBuffer Test
+			IntBuffer ib = mappedByteBuffer.asIntBuffer();
+			while (ib.hasRemaining()) {
+				System.out.println(ib.get());
+			}
+
+			// CharBuffer cBuffer = mappedByteBuffer.asCharBuffer();
+			// while (cBuffer.hasRemaining()) {
+			// System.out.println(cBuffer.get());
+			// }
+			// cBuffer.rewind();
+			// System.out.println(cBuffer.toString());
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO 自动生成的 catch 块
 			e.printStackTrace();
 		}
-		
+
 	}
-	public static void main(String[] args) {
-		RandAccessTest.write();
-		RandAccessTest.read();
+
+	private String getStr(){
+		return "lengjing";
+	}
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) throws  Exception{
+		// RandAccessTest.write();
+		// RandAccessTest.read();
+		RandAccessTest.bufferWrite();
 		RandAccessTest.bufferMap();
 	}
 }
