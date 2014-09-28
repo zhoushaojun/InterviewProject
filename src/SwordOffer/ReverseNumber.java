5277e38126cde83f66f941b54265bf3abcb98f6c
zhoushaojun <zhoushaojun2004@126.com>
2014-09-28 14:36:11 +0800

    add buma

--- src/SwordOffer/ReverseNumber.java
+++ src/SwordOffer/ReverseNumber.java
@@ -1,10 +1,11 @@
 package SwordOffer;
+
 /**
- * 利用二元归并 排序方法，从小到大排序。
- * 在生成的两部分进行比较：前半部分 大于 后半部分 某个值，那么 从后部分这个值开始到 后部分起点。均可构成逆序数
-		                               后半部分 大于 前半部分 某个值， 那么这个值 直接入库。无法构成逆序数。
+ * 利用二元归并 排序方法，从小到大排序。 在生成的两部分进行比较：前半部分 大于 后半部分 某个值，那么 从后部分这个值开始到 后部分起点。均可构成逆序数
+ * 后半部分 大于 前半部分 某个值， 那么这个值 直接入库。无法构成逆序数。
+ * 
  * @author zhoushaojun
- *
+ * 
  */
 public class ReverseNumber {
 	private static int[] data = { 7, 5, 6, 4 };
@@ -12,7 +13,7 @@ public class ReverseNumber {
 	public static void reverseEntry(int start, int end) {
 		if (start >= end)
 			return;
-		int mid = (start + end) / 2;
+		int mid = (end - start) / 2 + start;
 		reverseEntry(start, mid);
 		reverseEntry(mid + 1, end);
 		reverseCore(start, mid, end);
@@ -28,36 +29,42 @@ public class ReverseNumber {
 		int secondEnd = end;
 		int tempEnd = 0;
 		/**
-		 * logic: 前半部分 大于 后半部分 某个值，那么 从这个值开始到 起点。均可构成逆序数
-		 *        后半部分 大于 前半部分 某个值， 那么这个值 直接入库。无法构成逆序数。
+		 * logic: 前半部分 大于 后半部分 某个值，那么 从这个值开始到 起点。均可构成逆序数 后半部分 大于 前半部分 某个值， 那么这个值
+		 * 直接入库。无法构成逆序数。
 		 */
 		while (firstEnd >= firstStart && secondEnd >= secondStart) {
-			if(data[firstEnd] > data[secondEnd]){
+			if (data[firstEnd] > data[secondEnd]) {
 				tempEnd = secondEnd;
-				for(int i = tempEnd;i >= secondStart;i--){
-					printf(data[firstEnd],data[i]);
+				for (int i = tempEnd; i >= secondStart; i--) {
+					printf(data[firstEnd], data[i]);
 				}
-				temp[length--]=data[firstEnd];
+				temp[length--] = data[firstEnd];
 				firstEnd--;
-			}else{
-				temp[length--]=data[secondEnd];
+			} else {
+				temp[length--] = data[secondEnd];
 				secondEnd--;
 			}
 		}
-		while(firstEnd >= firstStart){
-			temp[length--]=data[firstEnd--];
+		while (firstEnd >= firstStart) {
+			temp[length--] = data[firstEnd--];
 		}
-		while(secondEnd >= secondStart){
-			temp[length--]=data[secondEnd--];
+		while (secondEnd >= secondStart) {
+			temp[length--] = data[secondEnd--];
 		}
 		System.arraycopy(temp, 0, data, start, temp.length);
 
 	}
-	private static void printf(int a,int b){
-		System.out.println("reverse number : "+a+" "+b);
+
+	private static void printf(int a, int b) {
+		System.out.println("reverse number : " + a + " " + b);
 	}
 
 	public static void main(String[] args) {
-		reverseEntry(0, 3);
+		//reverseEntry(0, 3);
+		int a= Integer.MAX_VALUE/2;
+		int b = Integer.MAX_VALUE/2+3;
+		int c= a+b;
+		System.out.println(c);
+		System.err.println(Integer.MAX_VALUE);
 	}
 }
