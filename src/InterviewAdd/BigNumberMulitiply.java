1f0735fd0573a277ecbca41edf47c256a337b64c
zhoushaojun <zhoushaojun2004@126.com>
2014-11-26 14:40:55 +0800

    modify get_min_value

--- src/InterviewAdd/BigNumberMulitiply.java
+++ src/InterviewAdd/BigNumberMulitiply.java
@@ -1,46 +1,49 @@
 package InterviewAdd;
+
 /**
  * ´óÊıÏà³Ë
+ * 
  * @author zhoushaojun
- *
+ * 
  */
 public class BigNumberMulitiply {
-	
+
 	private int bigNumber = 5;
-	
-	public void simulateBigNumberMulitiply(){
-		
+
+	public void simulateBigNumberMulitiply() {
+
 		int[] data = new int[10];
 		data[0] = 1;
 		int forward = 0;
 		int digital = 1;
-		//[1]The whole control for multiply of bignumber
-		for(int i=2; i<=bigNumber; i++){
-			
-			//[2]simulate the multiply process 
+		// [1]The whole control for multiply of bignumber
+		for (int i = 2; i <= bigNumber; i++) {
+
+			// [2]simulate the multiply process
 			forward = 0;
 			int temp = 0;
-			for(int j = 1; j<=digital; j++){
-				
-				temp = data[j-1]*i + forward;
-				data[j-1] = temp %10;
-				forward = temp /10;
+			for (int j = 1; j <= digital; j++) {
+
+				temp = data[j - 1] * i + forward;
+				data[j - 1] = temp % 10;
+				forward = temp / 10;
 			}
-			while(forward != 0){
-				data[digital++] = forward;
-				forward /=10;
+			while (forward != 0) {
+				data[digital++] = forward % 10;
+				forward /= 10;
 			}
-			
-		}//for end
-		
-		String  str = "";
+
+		}// for end
+
+		String str = "";
 		for (int i : data) {
-			str += i+"  ";
+			str += i + "  ";
 		}
 		System.out.println(str);
-		
+
 	}
-      public static void main(String[] args) {
+
+	public static void main(String[] args) {
 		BigNumberMulitiply big = new BigNumberMulitiply();
 		big.simulateBigNumberMulitiply();
 	}
--- src/InterviewCase/Get_min_value_use_heapsort.java
+++ src/InterviewCase/Get_min_value_use_heapsort.java
@@ -1,4 +1,5 @@
 package InterviewCase;
+
 /**
  * 最小K个值，堆排序方法
  */
@@ -11,134 +12,163 @@ import javax.naming.spi.DirStateFactory.Result;
 
 public class Get_min_value_use_heapsort {
 
-	private int[] data = {1,5,89,65,41,27,3,4,8,79};
-	
+	private int[] data = { 6, 1, 8, 2, 3, 9, 10, 12 };
+
 	TreeSet<Integer> result = new TreeSet<Integer>(new Comparator<Integer>() {
 
 		public int compare(Integer o1, Integer o2) {
 
-		   if(o1 < o2) return -1;
-		   if(o1 == o2 )return 0;
-		   else return 1;
+			if (o1 < o2)
+				return -1;
+			if (o1 == o2)
+				return 0;
+			else
+				return 1;
 		}
 
-	
 	});
-//////////////////////////////////////////////InterViewExtent///////////////////////////////////////////////////
-	
-	public void adjustHeap(int[] data , int index, int lenghth){
+
+	// ////////////////////////////////////////////InterViewExtent///////////////////////////////////////////////////
+
+	public void adjustHeap(int[] data, int index, int lenghth) {
 		int tempvalue = 0;
-		while(2*index+1 < lenghth){
-			int maxIndex = 2*index + 1;
-			if(2*index + 2 <lenghth){
-				if(data[2*index + 2] > data[2*index + 1]){
-					maxIndex = 2*index + 2;
-					this.addValue(data[2 * index + 1]);
-				}else{
-					this.addValue(data[2 * index + 2]);
+		while (2 * index + 1 < lenghth) {
+			int maxIndex = 2 * index + 1;
+			if (2 * index + 2 < lenghth) {
+				if (data[2 * index + 2] > data[2 * index + 1]) {
+					maxIndex = 2 * index + 2;
+				} else {
 				}
 			}
-			if(data[index] < data[maxIndex]){
+			if (data[index] < data[maxIndex]) {
 				tempvalue = data[index];
 				data[index] = data[maxIndex];
 				data[maxIndex] = tempvalue;
 				index = maxIndex;
-				this.addValue(data[index]);
+				// 添加元素(重要的一步，每次发生交换时候将　最少的两个 都加入数组)
+				// Index为交换后的　序列号，左子树或者右子树
+				this.addValueEnter(index, lenghth, true, data);
 				continue;
-				
-			}else{
+
+			} else {
 				break;
 			}
-			
-			
+
 		}
-		
+
 	}
-	public void createHeap(){
-		
+
+	public void createHeap() {
+
 		result.add(Integer.MAX_VALUE);
-		result.add(Integer.MAX_VALUE-1);
-		result.add(Integer.MAX_VALUE-2);
-		result.add(Integer.MAX_VALUE-3);
+		result.add(Integer.MAX_VALUE - 1);
+		result.add(Integer.MAX_VALUE - 2);
+		result.add(Integer.MAX_VALUE - 3);
 		int size = data.length;
-		for(int i = size/2-1; i >= 0; i--){
-			
-		     adjustHeap(data, i, size);	
+		for (int i = size / 2 - 1; i >= 0; i--) {
+
+			adjustHeap(data, i, size);
 		}
 		int tempvalue = 0;
-//		for(int i =size -1; i >= 0; i--){
-//			tempvalue = data[i];
-//			data[i] = data[0];
-//			data[0] = tempvalue;
-//			adjustHeap(data, 0, i);
-//			
-//		}
-		
+		// for(int i =size -1; i >= 0; i--){
+		// tempvalue = data[i];
+		// data[i] = data[0];
+		// data[0] = tempvalue;
+		// adjustHeap(data, 0, i);
+		//
+		// }
+
 	}
-	public void addValue(int value){
-		
-		if(result.size()!=0 && value < result.last() && !result.contains(value)){
-			System.out.println("zhou "+value + " "+result.last());
+	/**
+	 * Index 为 交换后的 序列号，左子树 或者 右子树。
+	 * 目标需要 将左子树 右子树 同时加入到数组中。 
+	 * 将Index/2 再进行 Index*2+1 Index*2+2 放入数组中
+	 * @param index
+	 * @param length
+	 * @param change
+	 * @param data
+	 */
+	private void addValueEnter(int index, int length, boolean change, int[] data) {
+		if (change) {
+			// 交换了数据
+			System.out.println("Index " + index);
+			index = index / 2;
+
+		}
+		if (2 * index + 1 < length) {
+			this.addValue(data[2 * index + 1]);
+		}
+		if (2 * index + 2 < length) {
+			this.addValue(data[2 * index + 2]);
+		}
+	}
+
+	private void addValue(int value) {
+
+		if (result.size() != 0 && value < result.last() && !result.contains(value)) {
+			// System.out.println("zhou " + value + " " + result.last());
 			Integer ints = result.last();
-			System.out.println(result.remove(ints));
+			result.remove(ints);
 			result.add(value);
 		}
-			
-		
+
 	}
-/////////////////////////////////////////////InterviewExtend End///////////////////////////////////////////////////////////////
-/////////////////////////////////////////////heapSort///////////////////////////////////////////////////////////////////////?/
-	public void adjustHeapInner(int[] data , int index, int lenghth){
+
+	// ///////////////////////////////////////////InterviewExtend
+	// End///////////////////////////////////////////////////////////////
+	// ///////////////////////////////////////////heapSort///////////////////////////////////////////////////////////////////////?/
+	public void adjustHeapInner(int[] data, int index, int lenghth) {
 		int tempvalue = 0;
-		while(2*index+1 < lenghth){
-			int maxIndex = 2*index + 1;
-			if(2*index + 2 <lenghth){
-				if(data[2*index + 2] > data[2*index + 1])
-					maxIndex = 2*index +2;
+		while (2 * index + 1 < lenghth) {
+			int maxIndex = 2 * index + 1;
+			if (2 * index + 2 < lenghth) {
+				if (data[2 * index + 2] > data[2 * index + 1])
+					maxIndex = 2 * index + 2;
 			}
-			if(data[index] < data[maxIndex]){
+			if (data[index] < data[maxIndex]) {
 				tempvalue = data[index];
 				data[index] = data[maxIndex];
 				data[maxIndex] = tempvalue;
 				index = maxIndex;
 				continue;
-				
-			}else{
+
+			} else {
 				break;
 			}
 		}
 	}
-	public void createHeapInner(){
+
+	public void createHeapInner() {
 		int size = data.length;
-		for(int i = size/2-1; i >= 0; i--){
-			
-		     adjustHeap(data, i, size);	
+		for (int i = size / 2 - 1; i >= 0; i--) {
+
+			adjustHeap(data, i, size);
 		}
 		int tempvalue = 0;
-		for(int i =size -1; i >= 0; i--){
+		for (int i = size - 1; i >= 0; i--) {
 			tempvalue = data[i];
 			data[i] = data[0];
 			data[0] = tempvalue;
 			adjustHeap(data, 0, i);
 		}
 	}
-//////////////////////////////////////////////heapSortEnd/////////////////////////////////////
+
+	// ////////////////////////////////////////////heapSortEnd/////////////////////////////////////
 	public static void main(String[] args) {
-		
-		
+
 		Get_min_value_use_heapsort min = new Get_min_value_use_heapsort();
 		min.createHeap();
-	
+
 		Iterator<Integer> iterator = min.result.iterator();
-		while(iterator.hasNext()){
+		while (iterator.hasNext()) {
 			System.out.println(iterator.next());
 		}
-//		min.createHeap();
-//		
-//		for(int v : min.data){
-//			
-//			System.out.println(v);
-//		}
-		
+		// min.createHeap();
+		//
+		// for(int v : min.data){
+		//
+		// System.out.println(v);
+		// }
+
 	}
 }
