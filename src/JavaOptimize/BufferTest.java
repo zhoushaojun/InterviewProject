f7a5c36a67fe77dfc72e4583e7a14d15519a8999
zhoushaojun <zhoushaojun2004@126.com>
2014-12-17 14:05:41 +0800

    rewind clear flip

--- src/JavaOptimize/BufferTest.java
+++ src/JavaOptimize/BufferTest.java
@@ -3,7 +3,13 @@ package JavaOptimize;
 import java.nio.Buffer;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
-
+/**
+ * 
+ * @author zhoushaojun
+ *  rewind use to read : position -> 0 ; limit don't move
+ *  clear  use to write : position - > 0; limit -> capacity
+ *  flip   use read write transfer : postion -> 0; limit -> position
+ */
 public class BufferTest {
 	
 	private static CharBuffer buffer = CharBuffer.allocate(1024);
@@ -11,18 +17,23 @@ public class BufferTest {
 		for (int i = 0; i < 10; i++) {
 			buffer.put('w');
 		}
-		buffer.flip();
+		//[1] rewind make position to 0, use to read
+		buffer.rewind();
 		char[] data = new char[4];
 		buffer.get(data);
 		for (char b : data) {
 			System.out.println(b);
 		}
-		data[0]= 1;
-		data[1]= 1;
-		data[2]= 1;
-		data[3]= 1;
-		buffer.flip();
-		buffer.put(data);
+		
+		//flip postion -> 0 limit->position : use read and write transfer
+		buffer.rewind();
+		buffer.put(0,'1');
+		buffer.put(1,'2');
+
+		buffer.rewind();
+		for (int i = 0; i < 10; i++) {
+			System.out.println(buffer.get(i));
+		}
 	}
 	public static void printf(){
 		System.out.println("postion : "+buffer.position());
@@ -31,9 +42,9 @@ public class BufferTest {
 	}
 	public static void main(String[] args) {
 		BufferTest.getTest();
-		BufferTest.printf();
-		String string = "java hello";
-		System.out.println(string.getBytes().length);
+		//BufferTest.printf();
+		//String string = "java hello";
+		//System.out.println(string.getBytes().length);
 		
 	}
 }
