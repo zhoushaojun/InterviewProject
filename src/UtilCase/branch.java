4272a6c55dd23e71f4ced79ec6b3e98354340108
zhoushaojun <zhoushaojun2004@126.com>
2015-02-10 14:23:58 +0800

    branch first

---
+++ src/UtilCase/branch.java
@@ -0,0 +1,34 @@
+package UtilCase;
+
+public class branch {
+	public void adjustHeap(int[] data, int index, int lenghth) {
+		int tempvalue = 0;
+		while (2 * index + 1 < lenghth) {
+			int maxIndex = 2 * index + 1;
+			if (2 * index + 2 < lenghth) {
+				if (data[2 * index + 2] > data[2 * index + 1]) {
+					maxIndex = 2 * index + 2;
+				} else {
+				}
+			}
+			if (data[index] < data[maxIndex]) {
+				tempvalue = data[index];
+				data[index] = data[maxIndex];
+				data[maxIndex] = tempvalue;
+				index = maxIndex;
+				// 添加元素(重要的一步，每次发生交换时候将　最少的两个 都加入数组)
+				// Index为交换后的　序列号，左子树或者右子树
+				continue;
+
+			} else {
+				break;
+			}
+
+		}
+
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
