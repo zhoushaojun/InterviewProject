a6db6a32dddf29351e7360f6f0e8afefdf3d76e4
zhoushaojun <zhoushaojun2004@126.com>
2015-02-10 14:22:18 +0800

    delete

--- src/UtilCase/wang.java
+++
@@ -1,9 +0,0 @@
-package UtilCase;
-
-public class wang {
-	public static void main(String[] args) {
-		System.out.println("roll7");
-		System.out.println("hard1");
-		System.err.println("hard3");
-	}
-}
--- src/UtilCase/zhou.java
+++
@@ -1,9 +0,0 @@
-package UtilCase;
-
-public class zhou {
-	public static void main(String[] args) {
-		System.out.println("1");
-		System.out.println("2");
-		System.out.println("merge");
-	}
-}
