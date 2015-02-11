2958d3a3a78a65dd036c67d5c1562d49bf9d5c1a
zhoushaojun <zhoushaojun2004@126.com>
2015-02-11 13:11:15 +0800

    111

--- src/UtilCase/branch.java
+++ src/UtilCase/branch.java
@@ -28,15 +28,11 @@ public class branch {
 				System.err.println("bug111");
 				System.out.println("bug222");
 				System.out.println("si505780push");
-				System.err.println("bug555");
+				System.err.println("bug555_bug111");
 				continue;
 
 			} else {
 				System.out.println("zhoushaojun_zsj");
-				System.out.println("develpe_zsj");
-				System.out.println("zhoushaojun_wh11");
-				System.out.println("develpe_wh11");
-				System.out.println("is550");
 				break;
 			}
 			
