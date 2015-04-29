f169b67a136d9011c880bab64a5023e59acfbcfb
zhoushaojun <zhoushaojun2004@126.com>
2015-04-29 17:35:05 +0800

    a

---
+++ src/DesignModule/Observer.java
@@ -0,0 +1,15 @@
+package DesignModule;
+
+public interface Observer {
+	/**
+	 * 推送形式，推送给用户一些 吸引信息
+	 * @param change
+	 */
+	public void updatePush(String updateStr);
+	
+	/**
+	 * 拉取模式，用户拉取更新的全部信息
+	 * @param concrete
+	 */
+	public void updatePull(ObservedAbstract concrete);
+}
