72eb56ed928096cf4fdac1a4631d3d8f91a0115e
zhoushaojun <zhoushaojun2004@126.com>
2015-07-02 10:23:25 +0800

    commit by 20150702

---
+++ src/CocurrentCollection/MapTest.java
@@ -0,0 +1,46 @@
+package CocurrentCollection;
+
+import java.util.LinkedHashMap;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class MapTest {
+	private static LinkedHashMap<String, String> lHashMapPut = new LinkedHashMap<String, String>(16,0.75f, false);
+	private static LinkedHashMap<String, String> lHashMapRead = new LinkedHashMap<String, String>(16,0.75f, true);
+	
+	public static void  lhashmapPutTest(){
+		lHashMapPut.put("1", "a");
+		lHashMapPut.put("2", "b");
+		lHashMapPut.put("3", "c");
+		
+		lHashMapPut.get("1");
+		
+		readMap(lHashMapPut);
+		
+	}
+	
+	
+	public static void  lhashmapReadTest(){
+		lHashMapRead.put("1", "a");
+		lHashMapRead.put("2", "b");
+		lHashMapRead.put("3", "c");
+		
+		lHashMapRead.get("2");
+		
+		readMap(lHashMapRead);
+		
+	}
+	
+	public static void readMap(LinkedHashMap< String,String> map){
+		Set<Entry<String, String>> keys = map.entrySet();
+		for (Entry<String, String> entry : keys) {
+			System.out.println(entry.getKey());
+			System.out.println(entry.getValue());
+		}
+	}
+	
+	public static void main(String[] args) {
+		//lhashmapPutTest();
+		lhashmapReadTest();
+	}
+}
--- src/GoogleGuava/GuavaTest.java
+++ src/GoogleGuava/GuavaTest.java
@@ -3,6 +3,7 @@ package GoogleGuava;
 import java.util.ArrayList;
 import java.util.List;
 
+
 import com.google.common.base.Objects;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Throwables;
@@ -10,6 +11,9 @@ import com.google.common.collect.Constraint;
 import com.google.common.collect.Constraints;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 
 public class GuavaTest {
 	 private static List<String> storeRoles = ImmutableList.of("AOrPa279");
@@ -49,11 +53,35 @@ public class GuavaTest {
 		ArrayList<Entity> imumuList = new ArrayList<Entity>();
 		imumuList.add(new Entity("zhou"));
 		List<String> googleImmu = ImmutableList.of("ad");
+		List<String> optionIds = ImmutableList.copyOf(new String[]{"a","b"});
+		List<Entity> optionIds5 = ImmutableList.copyOf(imumuList);
+		Entity entity = optionIds5.iterator().next();
+		System.out.println(entity.name);
+		entity.name ="lengjing";
+		System.out.println(optionIds5.iterator().next().name);
+		
+		//[7]
+		Lists.newArrayList();
+		Lists.newLinkedList();
+		Sets.newHashSet();
+		Sets.newTreeSet();
+		Maps.newHashMap();
+		Maps.newLinkedHashMap();
+		Maps.newTreeMap();
+		Maps.newConcurrentMap();
+		
 		
 	}
 	public static void main(String[] args) {
 		try {
 			test();	
+			GuavaTest test = new GuavaTest();
+			GuavaTest test1 = new GuavaTest();
+			System.out.println(test.getClass());
+			System.out.println(test1.getClass());
+			System.out.println(test.getClass() == test1.getClass());
+			
+			System.out.println(Integer.parseInt("ffff"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
